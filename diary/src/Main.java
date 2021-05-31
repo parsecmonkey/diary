c6791d7841a8a9dd0906cf8d2b8b3b63b46a92ef
momerio <mome.nakanishi@gmail.com>
2021-05-31 22:08:51 +0900

    window

--- diary/src/Main.java
+++ diary/src/Main.java
@@ -1,8 +1,12 @@
 package diary.src;
+
 import diary.src.*;
 
 class Main {
     public static void main(String args[]) {
         System.out.println("Hello world");
+        final int WINDOW_WIDTH = 800;
+        final int WINDOW_HEIGHT = 480;
+        new Window("日記", WINDOW_WIDTH, WINDOW_HEIGHT);
     }
 }
