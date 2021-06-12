f047a689cac50e39cf6e6ce5e887c564d5aab656
parsecmonkey <daisuke-1120@i.softbank.jp>
2021-06-12 23:50:12 +0900

    #5 ウィンドウにテキストエリアを設置

--- diary/src/EditWindow.java
+++ diary/src/EditWindow.java
@@ -1,7 +1,7 @@
 package diary.src;
 
 import javax.swing.*;
-// import java.awt.*;
+import java.awt.*;
 // import java.awt.event.*;
 
 public class EditWindow extends Window {
@@ -22,6 +22,21 @@ public class EditWindow extends Window {
         JLabel label = new JLabel("you clicked me");
         panel.add(label);
         add(panel);
+
+        getContentPane().add(panel, BorderLayout.PAGE_START);
+    }
+
+    // ウィンドウにテキストエリアを設置
+    public void setTextArea(String firstText, int columns) {
+        JTextArea textarea = new JTextArea(firstText); // テキストエリアの初期値を設定
+        textarea.setColumns(columns); // テキストエリアの列数を指定
+        textarea.setPreferredSize(new Dimension(200, 50)); // サイズを指定
+        textarea.setLineWrap(true); // 折り返し指定
+
+        JPanel p = new JPanel();
+        p.add(textarea);
+
+        getContentPane().add(p, BorderLayout.CENTER);
     }
 
 }
